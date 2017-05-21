0363299237f0968cfc39efdeefaabc242d2e86e2
mib1092 <mazurenko.ihor@gmail.com>
2017-05-21 14:08:41 +0300

    faq fixes

--- assets/js/custom/progressive-collapsibles.js
+++ assets/js/custom/progressive-collapsibles.js
@@ -4,29 +4,25 @@
 
 $('.accordion-list > li').each(function() {
 
-  var $this = $(this);
+  var $this = $(this),
+      h2    = $this.children('h2');
 
   // create unique id for a11y relationship
-
   var id = 'collapsible-' + $this.index();
 
   // wrap the content and make it focusable
-
   $this.find('.accordion-box-content').wrapAll('<div id="'+ id +'" aria-hidden="true">');
-  var panel = $this.children('h2').next();
-  console.log(panel);
+  var panel = h2.next();
 
   // Add the button inside the <h2> so both the heading and button semanics are read
-
-  $this.children('h2').wrapInner('<a href="#" aria-expanded="false" aria-controls="'+ id +'">');
-  var button = $this.find('h2 a');
+  h2.wrapInner('<a aria-expanded="false" aria-controls="'+ id +'">');
 
   // Toggle the state properties
-
-  button.on('click', function(e) {
+  h2.on('click', function(e) {
     e.preventDefault();
-    var state = $(this).attr('aria-expanded') === 'false' ? true : false;
-    $(this).attr('aria-expanded', state);
+    var a     = $(this).find('a'),
+        state = ( a.attr('aria-expanded') === 'false' ) ? true : false;
+    a.attr('aria-expanded', state);
     panel.attr('aria-hidden', !state);
   });
 
